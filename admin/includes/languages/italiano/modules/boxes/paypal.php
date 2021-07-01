fe1b6bfef80816b02ae0e4e00606228b526036df
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2021-07-01 12:12:57 +0200

    Add files via upload

---
+++ admin/includes/languages/italiano/modules/boxes/paypal.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULES_ADMIN_MENU_PAYPAL_HEADING', '<i data-content="Paypal" data-toggle="popover" data-placement="left" class="fab fa-paypal fa-fw mr-1"></i><span class="d-inline d-md-none">Paypal</span>');
+
+  define('MODULES_ADMIN_MENU_PAYPAL_BALANCE', 'Bilancio');
+  define('MODULES_ADMIN_MENU_PAYPAL_CONFIGURE', 'Configura');
+  define('MODULES_ADMIN_MENU_PAYPAL_LOG', 'Registro');
+  define('MODULES_ADMIN_MENU_PAYPAL_MANAGE_CREDENTIALS', 'Credenziali');
+  define('MODULES_ADMIN_MENU_PAYPAL_START', 'Inizia');
+
