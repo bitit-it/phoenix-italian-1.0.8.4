6e6dce29bf9d9c837a5c17368da6ec9b6471ba55
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2021-07-01 13:00:12 +0200

    Add files via upload

--- includes/languages/italiano/modules/payment/moneyorder.php
+++ includes/languages/italiano/modules/payment/moneyorder.php
@@ -2,16 +2,16 @@
 /*
   $Id$
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
 
-  Copyright (c) 2020 osCommerce
+  Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
 const MODULE_PAYMENT_MONEYORDER_TEXT_TITLE = 'Assegno/Contati';
-const MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION = 'Richiede un pagamento ooffline prima della spedizione dell\'ordine.';
+const MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION = 'Richiede un pagamento off-line prima della spedizione dell\'ordine.';
 const MODULE_PAYMENT_MONEYORDER_TEXT_CONFIRMATION = 'Rendere pagabile a:&nbsp;%1$s<br><br>Inviare a:<br>%2$s<br>%3$s<br><br>Il tuo ordine non verrà spedito fino a quando non riceveremo il pagamento.';
 const MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER = <<<'EOT'
 Rendere pagabile a: %1$s
