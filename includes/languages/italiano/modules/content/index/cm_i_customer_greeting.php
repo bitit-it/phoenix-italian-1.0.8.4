5e99c3743716a54666fd47593c50350eefe56480
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2021-07-01 12:42:24 +0200

    Add files via upload

--- includes/languages/italiano/modules/content/index/cm_i_customer_greeting.php
+++ includes/languages/italiano/modules/content/index/cm_i_customer_greeting.php
@@ -1,17 +1,17 @@
 <?php
 /*
-  $Id$ edit 18/11/2020
+  $Id$
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
 
-  Copyright (c) 2020 osCommerce
+  Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
 
-  const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Accesso Clienti';
+  const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Recensioni dei Clienti';
   const MODULE_CONTENT_CUSTOMER_GREETING_DESCRIPTION = 'Mostra le recensioni dei clienti nella home page del tuo sito';
 
   const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = 'Ben tornato <strong>%s!</strong> Vuoi vedere quale  <a role="button" class="btn btn-info btn-xs" href="%s">Nuovo Prodotto</a> è disponibile per l\'acquisto?';
