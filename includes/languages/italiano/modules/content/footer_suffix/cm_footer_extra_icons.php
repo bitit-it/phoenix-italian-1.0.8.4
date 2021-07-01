78be81d2f91acdf36fd3971fd3b633a0b5ba73d5
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2021-07-01 12:40:14 +0200

    Add files via upload

--- includes/languages/italiano/modules/content/footer_suffix/cm_footer_extra_icons.php
+++ includes/languages/italiano/modules/content/footer_suffix/cm_footer_extra_icons.php
@@ -2,16 +2,18 @@
 /*
   $Id$
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
 
-  Copyright (c) 2020 osCommerce
+  Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
   define('MODULE_CONTENT_FOOTER_EXTRA_ICONS_TITLE', 'Icone Pagamento');
-  define('MODULE_CONTENT_FOOTER_EXTRA_ICONS_DESCRIPTION', 'Aggiungi le incone di pagamento sotto il pie di pagina del tuo sito .<div class="alert alert-info">Le icone disponibili sono visionabili in: https://fontawesome.com/icons?d=gallery&s=brands&c=payments-shopping</div>');
+  const MODULE_CONTENT_FOOTER_EXTRA_ICONS_DESCRIPTION = <<<'EOT'
+  Aggiungi le incone di pagamento sotto il pie di pagina del tuo sito .<div class="alert alert-info">Le icone disponibili sono visionabili in: https://fontawesome.com/icons?d=gallery&s=brands&c=payments-shopping</div>
+EOT;
   
-  define('MODULE_CONTENT_FOOTER_EXTRA_ICONS_TEXT', '');
+  const MODULE_CONTENT_FOOTER_EXTRA_ICONS_TEXT = '';
 
