d2946adf692263f9343cb1d57d80afd3aea0f1b0
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2021-07-01 12:49:59 +0200

    Add files via upload

--- includes/languages/italiano/modules/content/login/cm_create_account_link.php
+++ includes/languages/italiano/modules/content/login/cm_create_account_link.php
@@ -2,13 +2,14 @@
 /*
   $Id$
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
 
-  Copyright (c) 2018 osCommerce
+  Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
+
 
   define('MODULE_CONTENT_CREATE_ACCOUNT_LINK_TITLE', 'Crea un account collegamento');
   define('MODULE_CONTENT_CREATE_ACCOUNT_LINK_DESCRIPTION', 'Mostra un contenitore di creare un account nella pagina di login');
@@ -16,3 +17,4 @@
   define('MODULE_CONTENT_LOGIN_HEADING_NEW_CUSTOMER', 'Nuovo Cliente ');
   define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER', 'Sono un nuovo cliente.');
   define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER_INTRODUCTION', 'Creando un nuovo account da ' . STORE_NAME . ' sarai in grado di acquistare velocemente, essere sempre aggiornato sullo stato dei tuoi ordini e tenere traccia degli ordini che hai effettuato.');
+
